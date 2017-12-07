df7876f8ea6482ad5d73de8e4556ce2ef0adf612
Monzurul Islam <monzurul.ce.buet@gmail.com>
2017-12-08 00:35:24 +0600

    navbar added

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	//scroll to top
+	/* $(window).scroll(function() {
+	    if ($(this).scrollTop() >= 50) {        
+	        $('#return-to-top').fadeIn(200); 
+	        $("#sticker").addClass("lightheader");
+	    } else {
+	        $('#return-to-top').fadeOut(200);  
+	        $("#sticker").removeClass("lightheader");
+	    }
+	});
+	$('#return-to-top').click(function() {    
+	    $('body,html').animate({
+	        scrollTop : 0          
+	    }, 500);
+	});*/
+
+
+	
+});
+
