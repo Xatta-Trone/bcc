915977b5d5c1939d5f43b672a2fb648c39b8f43c
Monzurul Islam <monzurul.ce.buet@gmail.com>
2017-12-21 19:03:40 +0600

    full responsive

--- js/custom.js
+++ js/custom.js
@@ -19,7 +19,8 @@ $(document).ready(function(){
 	$(".hero-slides, .programe-slides").owlCarousel({
 		items:1,
 		autoplay:true,
-		loop:true,	
+		loop:true,
+		dots:true,
 	});
 /*
 	$(".programe-slides").owlCarousel({
@@ -29,28 +30,47 @@ $(document).ready(function(){
 	});*/
 
 
-	$(".recent-news-slide").owlCarousel({
+	$(".recent-news-slide, .upcoming-news-slide").owlCarousel({
 		items:3,
+		responsive : {
+			320 :{items:1},
+            576 : { items : 2  }, // from zero to 480 screen width 4 items
+            768 : { items : 2  },
+            992 : { items: 3 }, // from 480 screen widthto 768 6 items
+            1024 :{ items : 3   // from 768 screen width to 1024 8 items
+            }
+        },
 		margin:5,
 		autoplay:false,
-		loop:true,	
+		loop:true,
+		nav:true,
+		navText:["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+
 	});
 
 	$(".company-logo-wrapper").owlCarousel({
 		items:5,
+		responsive : {
+			320 :{items:2},
+            576 : { items : 3  }, // from zero to 480 screen width 4 items
+            //768 : { items : 3  },
+            992 : { items: 4 }, // from 480 screen widthto 768 6 items
+            1024 :{ items : 5   // from 768 screen width to 1024 8 items
+            }
+        },
 		margin:20,
-		autoplay:false,
-		loop:true,	
+		autoplay:true,
+		loop:true,
+		autoplayTimeout:1100,
 	});
 
 	$('#clock').countdown('2018/01/01').on('update.countdown', function(event) {
   var $this = $(this).html(event.strftime(''
-    + '<span>%-w</span> week%!w '
-    + '<span>%-d</span> day%!d '
-    + '<span>%H</span> hr '
-    + '<span>%M</span> min '
-    + '<span>%S</span> sec'));
-});
+				+ '<div class="counter-container"><div class="counter-box first"><div class="number">%-D</div><span>Day%!d</span></div>'
+				+ '<div class="counter-box"><div class="number">%H</div><span>Hours</span></div>'
+				+ '<div class="counter-box"><div class="number">%M</div><span>Minutes</span></div>'
+				+ '<div class="counter-box last"><div class="number">%S</div><span>Seconds</span></div></div>'));
+	});
 
 
 	
